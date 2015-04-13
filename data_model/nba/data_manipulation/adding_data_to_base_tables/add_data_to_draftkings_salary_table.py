8b156516d13b99d8b75a41420c13fe06d827befc
jaebradley <jae.b.bradley@gmail.com>
2015-04-13 10:30:48 -0400

    draftkings insert

--- data_model/nba/data_manipulation/adding_data_to_base_tables/add_data_to_draftkings_salary_table.py
+++ data_model/nba/data_manipulation/adding_data_to_base_tables/add_data_to_draftkings_salary_table.py
@@ -1,4 +1,14 @@
 # Incomplete because Draftkings doesn't do a really good job at passing back the team that a player plays for
+import csv
+import os
+
+from sqlalchemy import create_engine, or_, and_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+
+from config import DRAFTKINGS_NBA
+from data_model.nba.model import Game, Player, DraftkingsPlayerSalary
+
 def add_data_to_draftkings_salary_table():
 
     mysql_connection = create_engine(URL(**DRAFTKINGS_NBA))
@@ -12,19 +22,17 @@ def add_data_to_draftkings_salary_table():
             draftkings_salary_list = list(reader)[1:]
             for player_salary in draftkings_salary_list:
 
-                position = player_salary[0]
                 first_name = player_salary[1]
                 last_name = player_salary[2]
                 salary = player_salary[3]
                 date = player_salary[4]
 
-                position_object = insert_session.query(Position).filter_by(abbreviation=position).one()
                 #should really query with a team object, but not passed back by draftkings so first_name and last_name SHOULD be enough
                 #might pick wrong player if more than one player has the same name
                 player_object = insert_session.query(Player).filter(first_name=first_name, last_name=last_name)
                 #home + away = team + opponent
                 #concat(home,away) = concat(team,opponent) or concat(opponent,team)
-                game_object = insert_session.query(Game).filter_by(date=date).one()
+                game_object = insert_session.query(Game).filter(and_(Game.date==date, or_(Game.home_team == player_object.team, Game.away_team == player_object.team))).one()
                 draftkings_salary_object = DraftkingsPlayerSalary(
                     player=player_object.id,
                     game=game_object.id,
