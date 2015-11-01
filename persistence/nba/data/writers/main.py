4be2143aa3e8b07c2a0a3d511052df92a3cc04c4
jaebradley <jae.b.bradley@gmail.com>
2015-11-01 17:33:48 -0500

    new files

--- persistence/nba/data/writers/main.py
+++ persistence/nba/data/writers/main.py
@@ -1,10 +1,13 @@
 from persistence.nba.data.writers.write_season_schedule import write_season_schedule_to_json
 from persistence.nba.data.writers.write_box_scores_for_season import write_box_scores_for_season_to_json
+import os
 
 
 def main(first_season_start_year, last_season_start_year):
+    dir = os.path.dirname(__file__)
     for season_start_year in range(first_season_start_year, last_season_start_year + 1):
-        write_season_schedule_to_json(season_start_year=season_start_year)
+        season_schedule_output_file = os.path.join(dir, '../static/schedules/{0}.json'.format(season_start_year))
+        write_season_schedule_to_json(season_start_year=season_start_year, output_file_path=season_schedule_output_file)
         write_box_scores_for_season_to_json(season_start_year=season_start_year)
 
 
--- persistence/nba/data/writers/write_season_schedule.py
+++ persistence/nba/data/writers/write_season_schedule.py
@@ -1,8 +1,10 @@
 from basketball_reference_web_scraper.readers import return_json_encoded_schedule
+import json
 
 
-def write_season_schedule_to_json(season_start_year):
+def write_season_schedule_to_json(season_start_year, output_file_path):
     json_encoded_schedule = return_json_encoded_schedule(season_start_year=season_start_year)
-    with open("../static/schedules/{0}.json".format(season_start_year)) as json_file:
+    print json_encoded_schedule
+    with open(output_file_path, "w+") as json_file:
         json_file.write(json_encoded_schedule)
         json_file.close()
